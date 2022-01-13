c9e1cd22f1ee7c2b324b792123309d72014b7d1f
William Richter <88400283+WRRicht3r@users.noreply.github.com>
2022-01-13 16:43:46 +0100

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Iroha 2 Tutorial",
   },
-  plugins: [
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-mdx",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+  plugins: [],
 };
